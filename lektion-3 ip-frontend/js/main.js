da96dddd06f49cfc2e04322de6cb7990d9caf1a4
Jqvistt <joakim.is.lonnqvist@gmail.com>
2026-04-01 10:21:45 +0300

    Implement IP fetching functionality and update DOM with client IP

--- lektion-3 ip-frontend/js/main.js
+++ lektion-3 ip-frontend/js/main.js
@@ -11,5 +11,8 @@ async function getMyIP(){
 
     const apiUrl = "https://jqvistt-rahti-demo-git-jockesprojekt.2.rahtiapp.fi/api/ip";
     const res = await fetch(apiUrl);
+    const data = await res.json();
+
+    document.getElementById("ip").textContent = data.client_ip;
 
 }
