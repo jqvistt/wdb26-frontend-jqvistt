b7a50d04b0f35cfe15c7e351af5c9415660e3f68
Jqvistt <joakim.is.lonnqvist@gmail.com>
2026-04-01 11:01:40 +0300

    Add functionality to fetch and display available rooms

--- lektion-3-ip-frontend/js/main.js
+++ lektion-3-ip-frontend/js/main.js
@@ -2,7 +2,7 @@ addEventListener("DOMContentLoaded", (event) => {
 
 
 getMyIP();
-
+getRooms();
 
 })
 
@@ -14,5 +14,20 @@ async function getMyIP(){
     const data = await res.json();
 
     document.getElementById("ip").textContent = data.client_ip;
+
+}
+
+async function getRooms(){
+
+    const apiUrl = "https://jqvistt-rahti-demo-git-jockesprojekt.2.rahtiapp.fi/api/rooms";
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+
+    const roomsList = document.getElementById("rooms");
+    data.rooms.forEach(room => {
+        const li = document.createElement("li");
+        li.textContent = `${room.room_number} - ${room.type} - ${room.price}€`;
+        roomsList.appendChild(li);
+    });
 
 }
